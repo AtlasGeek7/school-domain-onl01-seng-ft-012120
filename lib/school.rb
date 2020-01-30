102bfbb5076762f7f31067df87bbc6e94b3eb3c2
Khalid T <khalid.tissire@gmail.com>
2020-01-30 08:04:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+      if roster[grade]
+        roster[grade].push(student_name)
+      else
+        roster[grade] = []
+        roster[grade].push(student_name)
+      end
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+    hash = {}
+    roster.map { |g,s|  hash[g] = s.sort }
+    return hash
+  end
+end
